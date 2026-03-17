efe49948db0cbc9c82364a39ffa92d54ff5ac409
Pavitra <pavitraxsingh@gmail.com>
2026-03-17 21:49:36 +0530

    Maximum Depth of Binary Tree.c

---
+++ L104.c
@@ -0,0 +1,64 @@
+// Given the root of a binary tree, return its maximum depth.
+
+// A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+// Example 2:
+
+// Input: root = [1,null,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 104].
+// // -100 <= Node.val <= 100
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure (IMPORTANT in VS Code)
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// Create new node
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Function to find max depth
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+
+    return 1 + (left > right ? left : right);
+}
+
+int main() {
+    // Example: [3,9,20,null,null,15,7]
+
+    struct TreeNode* root = createNode(3);
+    root->left = createNode(9);
+    root->right = createNode(20);
+    root->right->left = createNode(15);
+    root->right->right = createNode(7);
+
+    printf("Maximum Depth: %d", maxDepth(root));
+
+    return 0;
+}
